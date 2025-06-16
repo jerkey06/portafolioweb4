73f3efac89db7bfea78a62e6e0967d8461105c1f
jerkey06 <ponceemiliano44@gmail.com>
2025-06-16 13:24:08 -0600

    Remove commented import for Blog section

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { ThemeProvider } from './context/ThemeContext';
 import { Header } from './components/layout/Header';
 import { Hero } from './components/sections/Hero';
 import { Projects } from './components/sections/Projects';
-import { Blog } from './components/sections/Blog';
+//import { Blog } from './components/sections/Blog';
 import { About } from './components/sections/About';
 import { GameOfLife } from './components/ui/GameOfLife';
 
